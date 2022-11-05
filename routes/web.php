e6a8b07597293684e681ad0cd4526b8a53ad05a7
Hans <Hansrafaalamri@gmail.com>
2022-11-05 19:05:41 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+/**
+ * @path /users
+ * @method GET
+ */
+Route::get('/users', [\App\Http\Controllers\UserController::class, 'index']);
+Route::get('/posts', [\App\Http\Controllers\UserController::class, 'index']);
